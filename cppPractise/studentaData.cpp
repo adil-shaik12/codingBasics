1b07df3bfcc1a377e960dd6dc5d71c1675284860
adil-shaik12 <smdadil156@gmail.com>
2025-10-08 22:43:13 +0530

    studentData

---
+++ cppPractise/studentaData.cpp
@@ -0,0 +1,19 @@
+//the below code is for student data
+#include<iostream>
+using namespace std;    
+
+int main(){
+    string studentName="adil";
+    int studentId=25;
+    int studentAge=19;
+    string course="btech";
+    int studentfee=35000;
+    cout<<"studentName:" <<studentName<<"\n";
+    cout<<"studentId:" <<studentId<<"\n";
+    cout<<"studentAge:" <<studentAge<<"\n";
+    cout<<"course:" <<course<<"\n";
+    cout<<"studentfee:" <<studentfee<<"\n";
+
+    
+
+}
