b2d0698f6079a09137a2e3bd725bb46a5e3928fa
adil-shaik12 <smdadil156@gmail.com>
2025-10-08 21:20:46 +0530

    learning hoe to declare variables

--- cppPractise/variables.cpp
+++ cppPractise/variables.cpp
@@ -5,7 +5,12 @@ using namespace std;
 int main(){
     int num=10;
     cout<<num;
+    char ch='a';
+    cout<<ch;   
+    string str="hello";
+    cout<<str;
+    int x,y,z=10;
+    cout<<x<<y<<z;
+
     return 0;
-
-
 }
