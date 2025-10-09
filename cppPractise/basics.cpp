8f7c0290a86dafea809fc71c2db4e68f0c2d39f8
adil-shaik12 <smdadil156@gmail.com>
2025-10-09 11:46:17 +0530

    code for operators

--- cppPractise/basics.cpp
+++ cppPractise/basics.cpp
@@ -53,11 +53,58 @@ int dataTypes(){
     cout<<num<<" "<<fee<<" "<<" "<<flag<<" "<<name<<" "<<sec<<" "<<data;
     return 0;
 }
+//the beow code is for operators
+//for arithmetic 
+int arithmetic(){
+    int a=10;
+    int b=5;
+    cout<<a+b<<"\n";
+    cout<<a-b<<"\n";
+    cout<<a*b<<"\n";
+    cout<<a/b<<"\n";
+    cout<<a%b<<"\n";
+    return 0;
+}
+int assignment(){
+    int x=3;
+     x += 5;
+     x-=1;
+     x*=5;
+     x/=3;
+     x%=2;
+     cout<<x;
+     return 0;
+}
+int comparison(){
+    int x=5;
+    int y=15;
+    cout<<(x==y);
+    cout<<(x!=y);
+    cout<<(x>y);
+    cout<<(x<y);
+    cout<<(x<=y);
+    cout<<(x>=y);
+    return 0;
+}
+//the below code is for logical
+int logical(){
+    int x=10;
+    int y=20;
+    cout<<(x < 12 && y>20);
+    cout<<(x<=10 || y<20);
+    cout<<(x!=20);
+    return 0;
+}
+
 
 int main(){
-    // output();
-    // variables();
-    // inputStudentData();
+    output();
+    variables();
+    inputStudentData();
     dataTypes();
+    arithmetic();
+    assignment();
+    comparison();
+    logical();
     return 0;
 }
