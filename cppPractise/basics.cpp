49a25d964883632ef785d0ca740327d7d05fc8f9
adil-shaik12 <smdadil156@gmail.com>
2025-10-09 10:55:36 +0530

    Added varaiables inputs to basics.cpp
    Added gitignore file for the project

--- cppPractise/basics.cpp
+++ cppPractise/basics.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 // the below code is for output
 int output(){
-    cout<<'output';
+    cout<<"output";
+    return 0;
+}
+//the below code is for learning variablaes in cpp
+int variables(){
+    int a;
+    char n;
+    string name;
+    cout<<a<<" "<<n<<" "<<name;
+    return 0;
+}
+//the below code is for learning inputs in cpp
+int inputStudentData(){
+    string name;
+    int age;
+    int studentId, rollNo;
+    cout<<"enter your name";
+    cin>>name;
+    cout<<"enter your age";
+    cin>>age;
+    cout<<"enter your studentId";
+    cin>>studentId;
+    cout<<"enter your rollNo";
+    cin>>rollNo;
+    cout<<name<<" "<<age<<" "<<studentId<<" "<<rollNo;
+    return 0;
 }
 
 int main(){
     output();
+    variables();
+    inputStudentData();
     return 0;
 }
