8fe7db9893cb7ffad3077b9585c82e94ff22120f
ameen-1436 <shaiksayeedameen@gmail.com>
2025-10-13 15:27:30 +0530

    showed adil how to create branches and how to push/publish them
    added arrayForLoop

--- cppPractise/basics.cpp
+++ cppPractise/basics.cpp
@@ -4,208 +4,247 @@
 using namespace std;
 
 // the below code is for output
-int output(){
-    cout<<"output";
+int output()
+{
+    cout << "output";
     return 0;
 }
-//the below code is for learning variablaes in cpp
-int variables(){
+// the below code is for learning variablaes in cpp
+int variables()
+{
     int a;
     char n;
     string name;
-    cout<<a<<" "<<n<<" "<<name;
+    cout << a << " " << n << " " << name;
     return 0;
 }
-//the below code is for learning inputs in cpp
-int inputStudentData(){
+// the below code is for learning inputs in cpp
+int inputStudentData()
+{
     string name;
     int age;
     int studentId, rollNo;
-    cout<<"enter your name";
-    cin>>name;
-    cout<<"enter your age";
-    cin>>age;
-    cout<<"enter your studentId";
-    cin>>studentId;
-    cout<<"enter your rollNo";
-    cin>>rollNo;
-    cout<<name<<" "<<age<<" "<<studentId<<" "<<rollNo;
+    cout << "enter your name";
+    cin >> name;
+    cout << "enter your age";
+    cin >> age;
+    cout << "enter your studentId";
+    cin >> studentId;
+    cout << "enter your rollNo";
+    cin >> rollNo;
+    cout << name << " " << age << " " << studentId << " " << rollNo;
     return 0;
 }
 // the below code is dataTypes
-int dataTypes(){
+int dataTypes()
+{
     int num;
     float fee;
-    bool flag=true;
+    bool flag = true;
     string name;
     char sec;
     double data;
-    cout<<"the num is:";
-    cin>>num;
-    cout<<"thr fee is:";
-    cin>>fee;
-    cout<<flag<<"\n";
-    cout<<"the name is:";
-    cin>>name;
-    cout<<"sec is:";
-    cin>>sec;
-    cout<<"the data is:";
-    cin>>data;
-    cout<<num<<" "<<fee<<" "<<" "<<flag<<" "<<name<<" "<<sec<<" "<<data;
+    cout << "the num is:";
+    cin >> num;
+    cout << "thr fee is:";
+    cin >> fee;
+    cout << flag << "\n";
+    cout << "the name is:";
+    cin >> name;
+    cout << "sec is:";
+    cin >> sec;
+    cout << "the data is:";
+    cin >> data;
+    cout << num << " " << fee << " " << " " << flag << " " << name << " " << sec << " " << data;
     return 0;
 }
-//the beow code is for operators
-//for arithmetic 
-int arithmetic(){
-    int a=10;
-    int b=5;
-    cout<<a+b<<"\n";
-    cout<<a-b<<"\n";
-    cout<<a*b<<"\n";
-    cout<<a/b<<"\n";
-    cout<<a%b<<"\n";
+// the beow code is for operators
+// for arithmetic
+int arithmetic()
+{
+    int a = 10;
+    int b = 5;
+    cout << a + b << "\n";
+    cout << a - b << "\n";
+    cout << a * b << "\n";
+    cout << a / b << "\n";
+    cout << a % b << "\n";
     return 0;
 }
-int assignment(){
-    int x=3;
-     x += 5;
-     x-=1;
-     x*=5;
-     x/=3;
-     x%=2;
-     cout<<x;
-     return 0;
-}
-int comparison(){
-    int x=5;
-    int y=15;
-    cout<<(x==y);
-    cout<<(x!=y);
-    cout<<(x>y);
-    cout<<(x<y);
-    cout<<(x<=y);
-    cout<<(x>=y);
+int assignment()
+{
+    int x = 3;
+    x += 5;
+    x -= 1;
+    x *= 5;
+    x /= 3;
+    x %= 2;
+    cout << x;
     return 0;
 }
-//the below code is for logical
-int logical(){
-    int x=10;
-    int y=20;
-    cout<<(x < 12 && y>20);
-    cout<<(x<=10 || y<20);
-    cout<<(x!=20);
+int comparison()
+{
+    int x = 5;
+    int y = 15;
+    cout << (x == y);
+    cout << (x != y);
+    cout << (x > y);
+    cout << (x < y);
+    cout << (x <= y);
+    cout << (x >= y);
     return 0;
 }
-//the below code is about strings
-int name(){
-    string name="adil";
-    cout<<"my name is:"<<name;
+// the below code is for logical
+int logical()
+{
+    int x = 10;
+    int y = 20;
+    cout << (x < 12 && y > 20);
+    cout << (x <= 10 || y < 20);
+    cout << (x != 20);
     return 0;
 }
-//the below code is for concatenation
-int concat(){
-    string fname="shaik";
-    string sname="mahammad";
-    string tname="adil";
-    cout<<fname<<" "+sname<<" "+tname;
+// the below code is about strings
+int name()
+{
+    string name = "adil";
+    cout << "my name is:" << name;
     return 0;
 }
-//code using append function
-int append(){
-    string fname="adil";
-    string sname="shaik";
+// the below code is for concatenation
+int concat()
+{
+    string fname = "shaik";
+    string sname = "mahammad";
+    string tname = "adil";
+    cout << fname << " " + sname << " " + tname;
+    return 0;
+}
+// code using append function
+int append()
+{
+    string fname = "adil";
+    string sname = "shaik";
     fname.append(sname);
-    cout<<fname;
+    cout << fname;
     return 0;
 }
-//the code is for adding numbers in strings
-int stringnum(){
-    string a="10";
-    string b="10";
-    a=a+b;
-    cout<<a;
+// the code is for adding numbers in strings
+int stringnum()
+{
+    string a = "10";
+    string b = "10";
+    a = a + b;
+    cout << a;
     return 0;
 }
-//the below code is for accessing strings,length and by using special characters
-int length(){
-    string txt="bhfbdhsgchsgdhcbh";
-    string nm="character";
-    string spe="my name is\"adil\"and i am from\\proddatur\\in \'kadapa'";
-    cout<<txt.size();
-    cout<<nm[0];
-    cout<<spe;
+// the below code is for accessing strings,length and by using special characters
+int length()
+{
+    string txt = "bhfbdhsgchsgdhcbh";
+    string nm = "character";
+    string spe = "my name is\"adil\"and i am from\\proddatur\\in \'kadapa'";
+    cout << txt.size();
+    cout << nm[0];
+    cout << spe;
     return 0;
 }
-//the below code is for user input string
-int input(){
+// the below code is for user input string
+int input()
+{
     string str;
-    cout<<"type your name:";
-    cin>>str;
-    cout<<"your full name is:";
+    cout << "type your name:";
+    cin >> str;
+    cout << "your full name is:";
     return 0;
 }
-//the below code is fro user input string using getline() method
-int get(){
+// the below code is fro user input string using getline() method
+int get()
+{
     string var;
-    cout<<"type your name:";
-    getline (cin,var);
-    cout<<"your full name is:"<<var;
+    cout << "type your name:";
+    getline(cin, var);
+    cout << "your full name is:" << var;
     return 0;
 }
-//the below code is for math using cmath header file
-int cmath(){
-    cout<<"square root of 49 is:"<<sqrt(49);
-    cout<<"round of a number 2.8 is :"<<round(2.8);
-    cout<<"natural logarithm of 3 is:"<<log(3);
-    cout<<"power of 2,3 is:"<<pow(2,3);
+// the below code is for math using cmath header file
+int cmath()
+{
+    cout << "square root of 49 is:" << sqrt(49);
+    cout << "round of a number 2.8 is :" << round(2.8);
+    cout << "natural logarithm of 3 is:" << log(3);
+    cout << "power of 2,3 is:" << pow(2, 3);
     return 0;
 }
-//the below code is ofr using statements
-int statement(){
-    int x=10;
-    int y=20;
-    if(x<y){
-        cout<<"x is lessthan y";
+// the below code is ofr using statements
+int statement()
+{
+    int x = 10;
+    int y = 20;
+    if (x < y)
+    {
+        cout << "x is lessthan y";
     }
-    else if(x<=y){
-        cout<<"x is equal to y";
-    }
-    
-    else{
-        cout<<"x is greater than y";
+    else if (x <= y)
+    {
+        cout << "x is equal to y";
     }
 
+    else
+    {
+        cout << "x is greater than y";
+    }
 }
-//the below code is for switch statement
-int switchStatement(){
-    int day=4;
-    switch (day){
-        case 1:
-        cout<<"monday";
+// the below code is for switch statement
+int switchStatement()
+{
+    int day = 4;
+    switch (day)
+    {
+    case 1:
+        cout << "monday";
         break;
-        case 2:
-        cout<<"tuesday";
+    case 2:
+        cout << "tuesday";
         break;
-        case 3:
-        cout<<"wednesday";
+    case 3:
+        cout << "wednesday";
         break;
-        case 4:
-        cout<<"thursday";
+    case 4:
+        cout << "thursday";
         break;
-        case 5:
-        cout<<"friday";
+    case 5:
+        cout << "friday";
         break;
-        case 6:
-        cout<<"saturday";
+    case 6:
+        cout << "saturday";
         break;
-        case 7:
-        cout<<"sunday";
+    case 7:
+        cout << "sunday";
         break;
         return 0;
     }
 }
 
-int main(){
+int arrayForLoop()
+{
+    int n;
+    cout << "enter the size of array";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
+int main()
+{
     output();
     variables();
     inputStudentData();
