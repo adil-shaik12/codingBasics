b2763ca87f402bb6d2dda367b0a0057a9b2cac75
adil-shaik12 <smdadil156@gmail.com>
2025-11-23 22:13:38 +0530

    written code for sorting elements in descending order in insertion sort algorithm

--- cppPractise/insertionSort.cpp
+++ cppPractise/insertionSort.cpp
@@ -24,7 +24,7 @@ void iSort1(vector<int>& arr){
         int j=i-1;
         while(j>=0 && arr[j]<key){
             arr[j+1]=arr[j];
-            j=j+1;
+            j=j-1;
         }
         arr[j+1]=key;
     }
